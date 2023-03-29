6e7a80a8693b5ccd26840f3971a2f6f63de6c374
taseenbappi <taseen.bappi@gmail.com>
2023-03-29 15:12:04 +0600

    hero section responsive issues fix

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-import "../../styles/Home/Hero.scss";
 import img from "../../Assets/images/hero-img.png";
 import { ReactComponent as ActionLive } from "../../Assets/images/live-action.svg";
+import "../../styles/Home/Hero.scss";
 
 const Hero = () => {
   return (
-    <div
-      id="hero"
-      className="container"
-      style={{ marginTop: "60px", marginBottom: "122px" }}
-    >
+    <div id="hero" className="container">
       <div className="row align-items-center">
         <div className="col-12 col-md-12 col-lg-6 col-sm-12">
           <h1 className="title-1 " style={{ marginBottom: "22px" }}>
             Discover, and collect Digital Art NFTs
           </h1>
-          <p className="body-1" style={{ marginBottom: "41px" }}>
+          <p className="body-1 hero-sub-title">
             Digital marketplace for crypto collectibles and non-fungible tokens
             (NFTs). Buy, Sell, and discover exclusive digital assets.
           </p>
           <button className="primary-btn">Explore Now</button>
-          <div className="d-flex" style={{ gap: "22px", marginTop: "30px" }}>
+          <div className="hero-summary-content">
             <div>
               <h1 className="title-1 m-0">98k+</h1>
               <p className="body-1">Artwork</p>
