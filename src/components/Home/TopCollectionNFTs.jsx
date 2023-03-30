065bd926e63d2e14a87173d97553d1ca61c26b78
taseenbappi <taseen.bappi@gmail.com>
2023-03-30 11:08:36 +0600

    TopCollectionNFT component responsive tablet  design updated

--- src/components/Home/TopCollectionNFTs.jsx
+++ src/components/Home/TopCollectionNFTs.jsx
@@ -7,19 +7,19 @@ import TopNFTsFeaturedCard from "./TopNFTsFeaturedCard";
 const TopCollectionNFTs = () => {
   return (
     <div className="container" id="top-collection-nft">
-      <div className="row g-4">
-        <div className="col-12 col-sm-12 col-md-12 col-lg-4">
+      <div className="row g-5">
+        <div className="col-12 col-sm-12 col-md-6 col-lg-4">
           <TopNFTsFeaturedCard />
         </div>
         <div
-          className="col-12 col-sm-12 col-md-12 col-lg-4 d-flex flex-column justify-content-between"
+          className="col-12 col-sm-12 col-md-6 col-lg-4 d-flex flex-column justify-content-between"
           style={{ gap: "10px" }}
         >
           <TopCollectionNFTsCard />
           <TopCollectionNFTsCard />
           <TopCollectionNFTsCard />
         </div>
-        <div className="col-12 col-sm-12 col-md-12 col-lg-4">
+        <div className="col-12 col-sm-12 col-md-10 col-lg-4 mx-auto">
           <TopCollectionOver />
         </div>
       </div>
